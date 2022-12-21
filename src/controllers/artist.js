0ed2b88b0e76f664ed3490ecce4ab22b68b0c103
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-12-21 22:13:17 +0000

    uploading changes

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,41 @@
-exports.create = (req, res) => {
-    res.sendStatus(201);
-}
+const db = require('../db/index');
+
+exports.create = async (req, res) => {
+  const { name, genre } = req.body;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      'INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *',
+      [name, genre]
+    );
+    res.status(201).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+exports.getAllArtists = async (_, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM Artists');
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+exports.getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rows: [artist] } = await db.query('SELECT * FROM Artists WHERE id = $1', [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const artistController = require('../controllers/artist')
+const express = require('express');
+const artistController = require('../controllers/artist');
 
 const router = express.Router();
 
-router.post("/", artistController.create);
+router.post('/', artistController.create);
+router.get('/', artistController.getAllArtists);
+router.get('/:id', artistController.getArtistById);
 
 module.exports = router;
